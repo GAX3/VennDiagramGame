01d7d4adbb7738b0c9e7261891972f283559776e
gusta <gangel@bytesw.com>
2024-12-05 16:02:40 -0600

    cambios iniciales

--- app/src/main/java/com/example/venndiagramgameapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/venndiagramgameapp/fragments/HomeFragment.kt
@@ -5,24 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.venndiagramgameapp.R
+import com.example.venndiagramgameapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Glide.with(this)
+            .asGif()
+            .load("https://cdn.pixabay.com/animation/2024/02/17/02/20/02-20-10-821_512.gif") // Replace with your GIF URL or local resource
+            .into(binding.gifImageView)
+
     }
 
 }
